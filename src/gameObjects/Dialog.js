93a3648176cc235356ecae3c935a38a3a41adb0e
Nikolaos Ftylitakis <ftylitak@gmail.com>
2022-07-01 00:09:52 +0300

    increase Dilog height to increase probability of fitting the whole text

--- src/gameObjects/Dialog.js
+++ src/gameObjects/Dialog.js
@@ -17,7 +17,7 @@ class Dialog {
         this.borderAlpha = opts.borderAlpha || 1
         this.windowAlpha = opts.windowAlpha || 0.8
         this.windowColor = opts.windowColor || 0x303030
-        this.windowHeight = opts.windowHeight || 200
+        this.windowHeight = opts.windowHeight || 220
         this.padding = opts.padding || 16
         this.closeBtnColor = opts.closeBtnColor || "darkgoldenrod"
         this.dialogSpeed = opts.dialogSpeed || 2
